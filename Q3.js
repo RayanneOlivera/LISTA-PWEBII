676628bbb5609bdf01d7aefcc84c5ead83116557
Rayanne Oliveira dos Santos <61166923+RayanneOlivera@users.noreply.github.com>
2023-10-10 20:07:43 -0300

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,7 @@
+let temperatura = 43;
+
+if (temperatura > 30) {
+    alert("A temperatura está acima de 30 graus Celsius.");
+} else {
+    alert("A temperatura não está acima de 30 graus Celsius.");
+}
