32d341477e9807c5419a729c4568a61d7750a5f0
Rayanne Oliveira dos Santos <61166923+RayanneOlivera@users.noreply.github.com>
2023-10-10 20:27:22 -0300

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,10 @@
+function soma(a, b) {
+    return a + b;
+}
+
+let numero1 = parseFloat(prompt("Insira o primeiro:"));
+let numero2 = parseFloat(prompt("Insira o segundo número:"));
+
+let resultado = soma(numero1, numero2);
+
+alert("A soma dos números é: " + resultado);
